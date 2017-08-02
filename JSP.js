3bbb0e20f2e29861828e053d6524a0c004cce89f
skovzky <rcheemann@hotmail.com>
2017-08-01 22:25:00 -0500

    JS and Streamlining
    Finally got around to working with Javascript in my site. Also have some streamlining for me with the menu bar bing its own HTML (JS, technically) that I can edit in one place.

--- JSP.js
+++ JSP.js
@@ -1,26 +1,13 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+function show_hide() {
+  var x = document.getElementById('exp-content');
+  var b = document.getElementById('button');
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  if(x.style.display ==='none'){
+    x.style.display = 'block';
+    b.value = "Close Text";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  else
+    x.style.display = 'none';   
+    b.value = "Open Text";
 }
+
---
+++ Menu.js
@@ -0,0 +1,28 @@
+document.write('\
+	<link rel="stylesheet" type="text/css" href="menu.css"/>\
+	<div id="nav-div">\
+			<li id="nav-li">\
+				<a href="index.html">Home</a>\
+			</li>\
+			<li id="nav-li">\
+				<a href="Thing_5.html" >School</a>\
+			</li>\
+			<li id="nav-li">\
+				<a href="Thing_1.html" >Cooking</a>\
+			</li>\
+			<li id="nav-li">\
+				<a id="music" href="Thing_2.html" >Music</a>\
+			</li>\
+			<li id="nav-li">\
+				<a href="Thing_3.html" >Games Ive Enjoyed</a>\
+			</li>\
+			<li id="nav-li">\
+				<a href="Thing_4.html" >Places Ive Been</a>\
+			</li>\
+			<li id="nav-li">\
+				<a href="Thing_6.html" >Photos</a>\
+			</li>\
+			<li id="nav-li">\
+			<a id="JS" href="js-test.html" >Js Playground</a>\
+			</li>\
+		</div>')
