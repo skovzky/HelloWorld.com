0a2b7c56d5c1aa6136b07cc7e03bb44fe86435f3
skovzky <rcheemann@hotmail.com>
2017-08-04 21:58:31 -0500

    Some changes
    Ive been working on this for a few days now. TBH im not even sure whats different. just lots of little things.

--- Menu.js
+++ Menu.js
@@ -6,22 +6,22 @@ document.write('\
 			</li>\
 			<li id="nav-li">\
 				<a href="Thing_5.html" >School</a>\
-			</li>\
+<!--		</li>\
 			<li id="nav-li">\
 				<a href="Thing_1.html" >Cooking</a>\
-			</li>\
+			</li>				-->\
 			<li id="nav-li">\
 				<a id="music" href="Thing_2.html" >Music</a>\
 			</li>\
-			<li id="nav-li">\
-				<a href="Thing_3.html" >Games Ive Enjoyed</a>\
+<!--		<li id="nav-li">\
+				<a href="Thing_3.html" >Games I\'ve Enjoyed</a>\
 			</li>\
 			<li id="nav-li">\
-				<a href="Thing_4.html" >Places Ive Been</a>\
+				<a href="Thing_4.html" >Places I\'ve Been</a>\
 			</li>\
 			<li id="nav-li">\
 				<a href="Thing_6.html" >Photos</a>\
-			</li>\
+			</li>				-->\
 			<li id="nav-li">\
 			<a id="JS" href="js-test.html" >Js Playground</a>\
 			</li>\
